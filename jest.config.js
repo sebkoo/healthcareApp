4d95ce205a793429695657b3403c3f6df5a4d405
Ben Koo <61488202+sebkoo@users.noreply.github.com>
2025-06-11 12:38:46 -0400

    test(auth): add unit tests for authentication service

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@react-native-firebase/(.*)$':
+      '<rootDir>/__mocks__/@react-native-firebase/$1.js',
+  },
 };
